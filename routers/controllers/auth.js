c5a277caa21067a4a7b05072b3fe96583638feb8
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-06-16 18:02:48 +0300

    login part 2

--- routers/controllers/auth.js
+++ routers/controllers/auth.js
@@ -31,10 +31,10 @@ const jwt = require('jsonwebtoken');
 // 			}
 // 		}
 // 		        finalR = ['The password you’ve entered is incorrect', 403];
-			
+
 // 		})
 //          	return finalR[0]
-	
+
 // };
 
 
@@ -43,35 +43,35 @@ const login = async (req, res) => {
 	console.log("email", email, "password", password)
 	// 
 	const query = `SELECT * FROM users WHERE email=?`
-		const data = [email];
-		
-	 	 db.query(query, data, async (err, results) => {
-			if (err) {
-				console.log(err)
-				console.log("no email")
+	const data = [email];
+	db.query(query, data, async (err, results) => {
+		if (err) {
+			console.log(err)
+			console.log("no email")
+		}
+		console.log("r", results[0].password)
+		if (results[0]) {
+			const valid = await bcrypt.compare(password, results[0].password);
+			if (valid) {
+				const payload = {
+					firstName: results[0].firstName,
+					lastName: results[0].lastName,
+					role: results[0].id,
+				};
+
+				console.log("paylod", payload, process.env.SECRET)
+				const options = {
+					expiresIn: '60m',
+				};
+				console.log("paylod", payload, process.env.SECRET, options)
+				const arr1= [jwt.sign(payload, process.env.SECRET, options), 200];
+				res.json(arr1[0]);
 			}
-			console.log("r", results[0].password)
-			if (results[0]) {
-				const valid = await bcrypt.compare(password, results[0].password);
-				if (valid) {
-					const payload = {
-						firstName: results[0].firstName,
-						lastName: results[0].lastName,
-						role: results[0].id,
-					};
-	
-					console.log("paylod", payload, process.env.SECRET)
-					const options = {
-						expiresIn: '60m',
-					};
-					console.log("paylod", payload, process.env.SECRET, options)
-	
-					res.json([jwt.sign(payload, process.env.SECRET, options), 200]);
-				}
-			}
-			     res.json( ['The password you’ve entered is incorrect', 403]);
-				
-			})
+		}
+		res.json(['The password you’ve entered is incorrect', 403]);
+        
+	})
+
 };
 module.exports = {
 	login,
